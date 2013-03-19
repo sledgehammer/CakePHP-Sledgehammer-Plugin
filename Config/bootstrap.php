e45364db5a21af07ed14c14d50b48c7b453a9251
Bob Fanger <bob.fanger@noprotocol.nl>
2013-03-19 16:13:56 +0100

    Removed overrides, CakePHP no longer includes a duplicate AppHelper, AppModel etc.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -28,6 +28,7 @@ if (isset($_SERVER['REQUEST_URI']) && $_SERVER['SCRIPT_FILENAME'] != WWW_ROOT.'t
 if ($_SERVER['SCRIPT_FILENAME'] == WWW_ROOT.'test.php') {
 	Framework::$autoloader->standalone = false; // PHPUnit also uses an autoloadeder
 }
+
 // Create CakePHP tmp folders
 \Sledgehammer\mkdirs(TMP.'cache/models');
 \Sledgehammer\mkdirs(TMP.'cache/persistent');
@@ -35,48 +36,10 @@ if ($_SERVER['SCRIPT_FILENAME'] == WWW_ROOT.'test.php') {
 \Sledgehammer\mkdirs(TMP.'logs');
 \Sledgehammer\mkdirs(TMP.'sessions');
 \Sledgehammer\mkdirs(TMP.'tests');
-//*
-// Don't show notices when a class is unknown to the AutoLoader (allow Cake to load or generate the class)
+
+// Don't show notices when a class is unknown to the AutoLoader (allow CakePHP to load or generate the class)
 Framework::$autoloader->standalone = false;
 App::uses('CakeModelWrapper', 'Sledgehammer.Model');
-/*/
-// Register CakePHP and Application classes to Sledgehammer's AutoLoader
-$ignoreFiles = array(
-	CAKE.'basics.php',
-	CAKE.'bootstrap.php',
-);
-$applicationOverrides = array(
-	'Controller/AppController.php',
-	'Controller/PagesController.php',
-	'Model/AppModel.php',
-	'View/Helper/AppHelper.php',
-);
-// Skip the class in Cake (if an override exists in the app folder
-foreach ($applicationOverrides as $applicationOverride) {
-	if (file_exists(APP.$applicationOverride)) {
-		$ignoreFiles[] = CAKE.$applicationOverride;
-	}
-}
-Framework::$autoLoader->importFolder(CAKE, array(
-	'mandatory_superclass' => false,
-	'ignore_folders' => array(CAKE.'Console', CAKE.'Test', CAKE.'TestSuite', CAKE.'Config'),
-	'ignore_files' => $ignoreFiles,
-	// CakePHP doesnt follow it's own standards...
-	'matching_filename' => false,
-	'one_definition_per_file' => false
-));
-Framework::$autoLoader->importFolder(APP, array(
-	'mandatory_definition' => false,
-	'ignore_folders' => array(
-		APP.'tmp',
-//		APP.'Config',
-	),
-	// Disable additional checks for vendor scripts
-	'detect_accidental_output' => false,
-	'matching_filename' => false,
-	'mandatory_superclass' => false,
-));
-// */
 
 // Use the Sledgehammer ErrorHandler
 Configure::write('Error.handler', array(Sledgehammer\Framework::$errorHandler, 'errorCallback'));
